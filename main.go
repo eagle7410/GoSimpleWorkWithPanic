df469c5ac91d7942c6e030816352892f5ff76dc8
Игорь (JS) <igor.js@hitech18.online>
2019-03-25 11:27:45 +0200

    To gogle fomat.

--- main.go
+++ main.go
@@ -12,17 +12,16 @@ func main() {
 		}
 	}()
 
-
 	fmt.Println(division(12, 3))
 	fmt.Println(division(12, 0))
 
 	panic("O my Good. Panic")
 }
 
-func division (base, part int) int {
+func division(base, part int) int {
 	if part == 0 {
 		panic("Division by zero")
 	}
 
-	return  base / part
+	return base / part
 }
